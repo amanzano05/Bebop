9aeaa8936c5fda9d03f2fad878ea83a7f68fdb04
Amanzano <amanzano05@icloud.com>
2019-10-31 19:20:01 +0000

    restore original

--- drone_start.sh
+++ drone_start.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
 #This is a very special script that was handcrafted by yours truly, Brandon Hernandez :)
 
 
 
-echo -e "Sourcing ros kinetic into bash..."
-        sleep 1
 
-source /opt/ros/kinetic/setup.bash
+echo -e "Sourcing the bebop workspace to the bash...."
+	sleep 2 
 
 source ~/bebop_ws/devel/setup.bash
 echo -e "Sourced!"
 
 echo -e "Launching the bebop driver..."
+	sleep 2
 
 xterm -e roslaunch bebop_driver bebop_node.launch & 
 
-	sleep 1 
+	sleep 8 
 
+echo -e "Sourcing ros kinetic into bash..."
+        sleep 2 
+
+source /opt/ros/kinetic/setup.bash
 echo -e "Sourced!"
+
 
 
